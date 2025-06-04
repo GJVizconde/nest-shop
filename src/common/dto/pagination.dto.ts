568add7a1db8ffe9ecf5fd0197896b010ae5cb59
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2025-06-04 05:13:19 -0400

    feat add ApiProperties in Dtos

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,12 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'Number of rows to return',
+  })
   @IsOptional()
   @IsPositive()
   @Type(() => Number) //enableImplicitConversions: true
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'Number of records to skip',
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number) //enableImplicitConversions: true
